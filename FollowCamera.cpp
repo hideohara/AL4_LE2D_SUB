e4252757af684641092f8679d2090b3caf1e239a
ohara@g.neec.ac.jp <ohara@g.neec.ac.jp>
2023-09-30 21:59:13 +0900

    01_03.自キャラの移動と追従カメラ_回転完成

--- FollowCamera.cpp
+++ FollowCamera.cpp
@@ -26,6 +26,14 @@ void FollowCamera::Update() {
 		// 追従対象からカメラまでのオフセット
 		Vector3 offset = {0.0f, 2.0f, -10.0f};
 
+		Matrix4x4 matRotX = MakeRotateXMatrix(viewProjection_.rotation_.x);
+		Matrix4x4 matRotY = MakeRotateYMatrix(viewProjection_.rotation_.y);
+		Matrix4x4 matRotZ = MakeRotateZMatrix(viewProjection_.rotation_.z);
+		// 回転行列の合成
+		Matrix4x4 matRot = matRotZ * matRotX * matRotY;
+
+		// オフセットをカメラの回転に合わせて回転させる
+		offset = TransformNormal(offset, matRot);
 		// 座標をコピーしてオフセット分ずらす
 		viewProjection_.translation_ = target_->translation_ + offset;
 	}
