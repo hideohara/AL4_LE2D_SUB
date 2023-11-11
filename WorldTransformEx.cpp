dfeb51ea202f688d09fb8fc431ba02e5fcf4627a
大原英郁 <ohara@g.neec.ac.jp>
2023-11-11 17:23:12 +0900

    UpdateMatrix　説明付き

--- WorldTransformEx.cpp
+++ WorldTransformEx.cpp
@@ -1,6 +1,10 @@
 ﻿#include "MathUtilityForText.h"
 #include "WorldTransform.h"
 
+// WorldTransform.h に関数宣言を加える
+// worldTransform_.UpdateMatrix();
+// のように呼び出せば、親子構造を含めた行列変換と転送を行う
+
 void WorldTransform::UpdateMatrix() {
 	// スケール、回転、平行移動を合成して行列を計算する
 	matWorld_ = MakeAffineMatrix(scale_, rotation_, translation_);
